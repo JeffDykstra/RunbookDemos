9fcd47db0bfebbfe024b47333e202f54e26e8dee
Jeff Dykstra <JeffDyk@microsoft.com>
2023-02-02 20:51:39 -0600

    Removed old code from Logic App/Form Demo

--- ProcessMSFormInput.ps1
+++ ProcessMSFormInput.ps1
@@ -8,10 +8,3 @@ param(
 Write-verbose  "Start of runbook on: $($ENV:computername)"
 Write-Warning  "Start of runbook on: $($ENV:computername)"
 [pscustomobject]$PSBoundParameters | ConvertTo-Json
-	<#
-	$input1
-	$input2
-	$date
-	$EmailofSubmitter
-	$SubmissionTime
-	#>
