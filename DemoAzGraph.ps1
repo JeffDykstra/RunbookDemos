acbc4d1a352452fb84ed84ee6f88bb63a29a214f
Jeff Dykstra <JeffDyk@microsoft.com>
2023-02-16 19:51:40 -0600

    Changed AZ Grpah demo to output json arrays.

--- DemoAzGraph.ps1
+++ DemoAzGraph.ps1
@@ -1,18 +1,21 @@
 #Requires -module Az.ResourceGraph
+param([string]$EnvironmentTag='PRD')
 Connect-AzAccount -Identity
-$query = @'
+$ResoureGroupQuery = @"
 resourcecontainers
-| where type == "microsoft.resources/subscriptions/resourcegroups"
+| where type == "microsoft.resources/subscriptions/resourcegroups" and tags["Environment Type"] == '$EnvironmentTag'
 | join kind =inner (resources
 | where type == "microsoft.compute/virtualmachines" 
 | extend osType = properties.storageProfile.osDisk.osType 
 | project resourceGroup,name,osType 
 | summarize LinuxVMs=countif(osType=='Linux'),WindowsVMs=countif(osType=='Windows') by resourceGroup) on resourceGroup
-| extend Environment = tags.Environment
+| extend Environment = tags["Environment Type"]
 | extend AppOwner = tags["Application Owner"]
 | extend boolLinuxVM = tobool(case(LinuxVMs >0, "True","False"))
 | extend boolWindowsVM = tobool(case(WindowsVMs >0, "True","False"))
-| project name,resourceGroup,subscriptionId,Environment,AppOwner,id,boolLinuxVM,boolWindowsVM
-'@
-$RgswithVMinfo = Search-AzGraph -Query $query -UseTenantScope
-$RgswithVMinfo | ogv
+| join kind = leftouter ( resources | where type== 'microsoft.insights/metricalerts' or type== 'microsoft.insights/activitylogalerts' or type== 'microsoft.insights/scheduledqueryrules'| summarize DefaultRuleCount = count(),rulelist=make_list(name) by resourceGroup) on resourceGroup 
+| project name,resourceGroup,location,subscriptionId,LinuxVMs,WindowsVMs,Environment,AppOwner,boolLinuxVM,boolWindowsVM,id,DefaultRuleCount,rulelist 
+| sort by subscriptionId
+"@
+$RgswithVMinfo = Search-AzGraph -Query $ResoureGroupQuery -UseTenantScope
+$RgswithVMinfo | ConvertTo-Json
